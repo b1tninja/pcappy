a44e3c6ed81b0cccb8a75ae4ec5ff7198b876d9b
Justin Capella <justincapella+b1tninja@gmail.com>
2015-06-26 16:52:17 -0700

    Fixed up the handling of 8021q and 8021ad frames, added a vlan tag class/object

--- pcap.py
+++ pcap.py
@@ -167,9 +167,6 @@ class UDP(Packet):
         except AssertionError:
             pass
 
-
-
-
     def __repr__(self):
         return "<UDP src=%d, dst=%d, length=%d, checksum=%s, payload=%s>" % (self.src, self.dst, self.length, self.checksum, self.data)
 
@@ -275,9 +272,23 @@ class ARP(Layer3):
 class Frame(object):
     pass
 
+class ieee8021q_header(object):
+    def __init__(self, pcp, dei, vlan):
+        self.pcp = pcp
+        self.dei = dei
+        self.vlan = vlan
 
-class Ethernet(Frame):
-    parsers = dict([(struct.pack('!H', getattr(parser,'ether_type')), parser) for parser in Layer3.__subclasses__()])
+    @classmethod
+    def parse(cls, buffer):
+        assert len(buffer) == 2
+        pcp = buffer[0] >> 5
+        dei = bool(buffer[0] & 0b00010000)
+        vlan = ((buffer[0] & 0b00001111) << 4) | buffer[1]
+        return cls(pcp,dei,vlan)
+
+
+class Ethernet2(Frame):
+    parsers = dict([(getattr(parser,'ether_type'), parser) for parser in Layer3.__subclasses__()])
 
     def __init__(self, dst, src, ether_type, data=b''):
         self.dst = dst
@@ -288,30 +299,32 @@ class Ethernet(Frame):
 
     @classmethod
     def parse(cls, payload):
+        # TODO: add some sanity checking for lengths / padding
         dst, src = (payload[0:6], payload[6:12])
-        ether_type = struct.unpack_from('!H', payload, 12)
-        if ether_type == 0x8100:
-            # assert len(payload) >= 38 TODO: consult 8023ac
-            pcp = payload[14] >> 5
-            dei = bool(payload[14] & 0b00010000)
-            vlan = ((payload[14] & 0b00001111) << 4) | payload[15]
+        # ether_type = struct.unpack_from('!H', payload, 12)
+        ether_type = payload[12] << 8 | payload[13]
+        if ether_type == ieee8021q.tpid:
+            tag = ieee8021q_header.parse(payload[14:16])
             ether_type = payload[16:18]
             data = payload[18:]
-
-        elif ether_type == 0x88a8:
-            # assert len(payload) >= 34
-            stag, ctag, ether_type, data = (payload[14:16], payload[16:20], payload[20:22], payload[22:])
+            return ieee8021q(dst, src, tag, ether_type, data)
+        elif ether_type == ieee8021ad.tpid:
+            stag = ieee8021q_header.parse(payload[14:16])
+            ctag = ieee8021q_header.parse(payload[16:18])
+            ether_type = payload[18:20]
+            data = payload[20:]
+            return ieee8021ad(dst, src, stag, ctag, ether_type, data)
         else:
-            ether_type, data = (payload[12:14], payload[14:])
-            # assert len(payload) >= 46
-
-        # TODO: decide what to do with vlan info... currently just throwing it away
-        return cls(dst, src, ether_type, data)
+            data = payload[14:]
+            return cls(dst, src, ether_type, data)
 
     def get_datagram(self):
         if self.datagram is False:
             if self.ether_type in self.parsers:
-                self.datagram = self.parsers[self.ether_type].parse(self.data)
+                try:
+                    self.datagram = self.parsers[self.ether_type].parse(self.data)
+                except:
+                    pass
             else:
                 self.datagram = None
 
@@ -320,29 +333,44 @@ class Ethernet(Frame):
     def __repr__(self):
         return "<Frame src=%s dst=%s len=%d>" % (binascii.hexlify(self.dst), binascii.hexlify(self.src), len(self.data))
 
-class ieee8021q(Frame):
-    #TODO: consider subclassing ethernet for the vlan stuff?
-    pass
+class ieee8021q(Ethernet2):
+    tpid = 0x8100
+
+    def __init__(self, dst, src, tag, ether_type, data):
+        super(ieee8021q, self).__init__(dst, src, ether_type, data)
+        self.tag = tag
+
+class ieee8021ad(Ethernet2):
+    tpid = 0x88a8
+
+    def __init__(self, dst, src, stag, ctag, ether_type, data):
+        super(ieee8021ad, self).__init__(dst, src, ether_type, data)
+        self.stag = stag
+        self.ctag = ctag
 
 
-class ieee8021ad(Frame):
+class ArpTable(object):
     pass
 
 
 class RawSocket(asyncio.Protocol):
     def connection_made(self, transport):
         self.transport = transport
+        self.arp_table = ArpTable()
 
     def data_received(self, data):
         assert len(data) >= 14
-        frame = Ethernet.parse(data)
+        frame = Ethernet2.parse(data)
         datagram = frame.get_datagram()
-        print(frame, datagram)
+        #print(frame, datagram)
 
-        # TODO: Abstract for generic layer3?
+        if isinstance(datagram, ARP):
+                print(frame, datagram)
+
         if isinstance(datagram, IP):
+            # print(frame, datagram, datagram.packet)
             if isinstance(datagram.packet, ICMP):
-                print(datagram.packet)
+                print(frame, datagram, datagram.packet)
 
 
 if __name__ == '__main__':
